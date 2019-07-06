8d3b8314d3d04688026979820f216288f58dd2e5
Margaret Bogdanou <margbogd@gmail.com>
2019-07-06 15:25:13 +0300

    Updated

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  getQuote();
+  
+var randomQuote;
+var author;
+function getQuote(){
+ 
+  
+  var url="https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+  $.getJSON(url, function(data){
+   $(".quote-text").html('"'+data.quoteText+'"');
+   $(".author").html("-"+data.quoteAuthor);
+    
+  });
+};
+  $("#tweet-quote").on("click", function(){
+      console.log(randomQuote);
+window.open("https://twitter.com/intent/tweet?text=" + randomQuote);
+  });
+  $("#tweet-quote").on("click", function(){
+      console.log(randomQuote);
+window.open("https://twitter.com/intent/tweet?text=" + randomQuote);
+  });
+  
+  $("#new-quote").on("click", function(){
+    getQuote();
+  });
+});
